0a20aff9a5d2ffd314bab73dd348a51dbeed776d
Skylan Lew <skylanslew@gmail.com>
2022-09-23 16:54:12 -0700

    update Program

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,33 +1,48 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Bakery
 {
     public class Program
     {
       public static void Main(){
+        Console.Clear();
         StartMenu();
 
       }
 
       private static void StartMenu(){
-        Console.WriteLine("     Welcome to Pierre's Bakery     ");
+        Console.WriteLine("*----------------------------------*");
+        Console.WriteLine("|    Welcome to Pierre's Bakery    |");
         Console.WriteLine("*----------------------------------*");
         Console.WriteLine("|Bread...........................$5|");
         Console.WriteLine("|Pastry..........................$2|");
         Console.WriteLine("|                                  |");
         Console.WriteLine("|Bread: B2G1 Free   Pastry: B2G1 $1|");
         Console.WriteLine("*----------------------------------*");
-        Console.WriteLine("Press any key to continue...");
-        Console.ReadKey();
-
-        BuyBread();
+        Console.WriteLine("Press any key to continue, esc to leave...");
+        if(Console.ReadKey().Key == ConsoleKey.Escape)
+        {
+          Console.Clear();
+          Console.WriteLine(" B"); //I need this line in order for the rest of the lines to show up, for some reason
+          Console.WriteLine("Pierre: \"Goodbye.\"");
+          Console.WriteLine("You leave.");
+        } 
+        else 
+        {
+          BuyBread();
+        }
       }
       private static void BuyBread()
       {
-        Console.WriteLine("Please enter how many loaves of bread you would like to buy:");
+        Console.Clear();
+        Console.WriteLine("*----------------------------------*");
+        Console.WriteLine("|Bread...........................$5|");
+        Console.WriteLine("|                                  |");
+        Console.WriteLine("|Bread: B2G1 Free                  |");
+        Console.WriteLine("*----------------------------------*");
+        Console.WriteLine("How many loaves of bread you would like to buy?");
         int breadToBuy;
         bool success = int.TryParse(Console.ReadLine(), out breadToBuy);
         if (success)
@@ -39,12 +54,16 @@ namespace Bakery
           Console.WriteLine("Invalid Selection, please try again.");
           BuyBread();
         }
-
-        
       }
       private static void BuyPastry(int breadToBuy)
       {
-        Console.WriteLine("Please enter how many pastries you would like to buy:");
+        Console.Clear();
+        Console.WriteLine("*----------------------------------*");
+        Console.WriteLine("|Pastry..........................$2|");
+        Console.WriteLine("|                                  |");
+        Console.WriteLine("|Pastry: B2G1 $1                   |");
+        Console.WriteLine("*----------------------------------*");
+        Console.WriteLine("How many pastries you would like to buy?");
         int pastryToBuy;
         bool success = int.TryParse(Console.ReadLine(), out pastryToBuy);
         if (success)
@@ -71,8 +90,6 @@ namespace Bakery
         int pastryLength = 22 - buyPastry.TotalCost.ToString().Length;
         int totalLength = 25 - total.ToString().Length;
 
-        Console.WriteLine("Bread length:" + breadLength.ToString());
-
         string breadLine = "";
         string pastryLine = "";
         string totalLine = "";
@@ -81,10 +98,13 @@ namespace Bakery
         for(int i = 0; i < pastryLength - checkOut[1].ToString().Length; i++){ pastryLine += ".";}
         for(int i = 0; i< totalLength; i++){ totalLine += ".";}
 
-        Console.WriteLine("Bread line:" + breadLine);
+        Console.Clear();
         
         Console.WriteLine("*----------------------------------*");//34
         Console.WriteLine("|             Reciept              |");
+        Console.WriteLine("|          Pierres Bakery          |");
+        Console.WriteLine("|          1234 Center St          |");
+        Console.WriteLine("|     East Grestin, Arstotzka      |");
         Console.WriteLine("*----------------------------------*");
         Console.WriteLine("|Bread x" + checkOut[0]  + breadLine + "$"+ buyBread.TotalCost.ToString() +".00|");
         Console.WriteLine("|Pastry x" + checkOut[1] +  pastryLine + "$"+ buyPastry.TotalCost.ToString() +".00|");
@@ -93,10 +113,17 @@ namespace Bakery
         Console.WriteLine("*----------------------------------*");
         Console.WriteLine("|        Glory To Arstotzka        |");
         Console.WriteLine("*----------------------------------*");
-        string response = Console.ReadLine().ToUpper();
-        if(response == "GLORY TO ARSTOTZKA" || "GLORY TO ARSTOTZKA!")
+        Console.WriteLine("Press any key to exit...");
+        if(Console.ReadKey().Key == ConsoleKey.G )
         {
           EasterEgg();
+        }
+        else
+        {
+          Console.Clear();
+          Console.WriteLine("You lose $" + total);
+          Console.WriteLine("Pierre: \"Glory to Arstotzka\"");
+          Console.WriteLine("You leave with your bread.");
         }
       }
 
