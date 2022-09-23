0c74064fc64ec8372832fa5e8eac4e78e79b1eec
Skylan Lew <skylanslew@gmail.com>
2022-09-23 10:33:15 -0700

    pass test 2

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -18,7 +18,10 @@ namespace Bakery
     public void CalcCost() //buy 2 get 1 free
     {
       int totalCost = 0;
-      totalCost += 2;
+      for(int i = 0; i < ToBuy; i++)
+      {
+        totalCost += _cost;
+      }
       TotalCost = totalCost;
     }
   }
