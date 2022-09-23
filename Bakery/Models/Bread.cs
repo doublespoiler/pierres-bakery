0ae77962ac6a09e390009f26072b8d294232562d
Skylan Lew <skylanslew@gmail.com>
2022-09-23 10:15:10 -0700

    initial class setup

---
+++ Bakery/Models/Bread.cs
@@ -0,0 +1,20 @@
+namespace Bakery
+{
+  public class Bread
+  {
+    public int Cost { get; set; }
+    public int ToBuy { get; set; }
+
+    public Bread(int toBuy)
+    {
+      ToBuy = toBuy;
+      Cost;
+    }
+
+    private int CalcCost()
+    {
+      int totalCost = 0;
+      return totalCost;
+    }
+  }
+}
---
+++ Bakery/Models/Pastry.cs
@@ -0,0 +1,19 @@
+namespace Bakery
+{
+  public class Pastry
+  {
+    public int Cost { get; set; }
+    public int ToBuy { get; set; }
+
+    public Pastry(int toBuy)
+    {
+      ToBuy = toBuy;
+      Cost;
+    }
+    private int CalcCost()
+    {
+      int totalCost = 0;
+      return totalCost;
+    }
+  }
+}
